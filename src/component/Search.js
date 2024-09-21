7e554e7dd00faf40a4a0e2cfbf771d2c5384b960
Luhar-Jay <luharjay67@gmail.com>
2024-09-21 10:31:40 +0530

    deploy app on firebase

--- src/component/Search.js
+++ src/component/Search.js
@@ -1,148 +1,142 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import RestaurentCard from "./RestaurentCard";
+// import RestaurentCard from "./RestaurentCard";
 import { CDN_URL } from "../utils/constant";
+import Category from "./Category";
 
 const Search = () => {
   const [searchText, setSearchText] = useState("");
   const [listOfRestaurent, setListOfRestaurent] = useState([]);
-  const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+  // const [filteredRestaurants, setFilteredRestaurants] = useState([]);
   const [suggetions, setSuggetions] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
 
   useEffect(() => {
-    fetchData();
+    fetchData("");
   }, []);
 
-  const fetchData = async () => {
+  const fetchData = async (searchText) => {
     const response = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.022505&lng=72.5713621&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=23.022505&lng=72.5713621&str=${searchText}`
     );
     const json = await response.json();
-    console.log(json.data);
-    setListOfRestaurent(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
+    console.log(json?.data?.suggestions);
+    setListOfRestaurent(json?.data);
   };
+  console.log("listOfRestaurent", listOfRestaurent);
 
-  const handleSearch = () => {
-    const filteredRestaurant = listOfRestaurent.filter((res) =>
-      res.info.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setFilteredRestaurants(filteredRestaurant);
-
-    const suggested = listOfRestaurent.filter((res) =>
-      res.info.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setSuggetions(suggested);
-  };
   const handleSearchTextChange = (e) => {
     const searchText = e.target.value;
     setSearchText(searchText);
-
-    const suggested = listOfRestaurent.filter((res) =>
-      res.info.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setSuggetions(suggested);
+    if (listOfRestaurent && listOfRestaurent.length > 0) {
+      const suggested = listOfRestaurent.filter((res) =>
+        res.suggestions[0].text.toLowerCase().includes(searchText.toLowerCase())
+      );
+      setSuggetions(suggested);
+    } else {
+      fetchData(searchText);
+    }
   };
   return (
-    <div className="max-w-[1250px] mx-auto">
-      <div className="justify-center flex mx-auto items-center text-center">
-        <input
-          type="text"
-          data-testid="searchInput"
-          placeholder="Search for restaurant and food"
-          className="border p-2 border-solid border-black rounded h-9 w-[550px]"
-          value={searchText}
-          onChange={handleSearchTextChange}
-          onKeyUp={(e) => {
-            if (e.key === "Enter") {
-              handleSearch();
-            }
-          }}
-          onFocus={() => setSuggetions(true)}
-          onBlur={() => setTimeout(() => setSuggetions(false), 200)}
-        />
-        <button
-          className="px-4 py-2 bg-green-200 m-4 rounded-lg"
-          onClick={handleSearch}
-        >
-          Search
-        </button>
-        <div className="bg-white text-black">
-          {suggetions.length > 0 && (
-            <ul className="absolute bg-white top-[150px] left-[305px] border z-10 w-[550px] p-2 ">
+    <>
+      <div className="max-w-[1250px] mx-auto">
+        <div className="justify-center flex mx-auto items-center text-center">
+          <input
+            type="text"
+            data-testid="searchInput"
+            placeholder="Search for restaurant and food"
+            className="border p-2 border-solid border-black rounded h-9 w-[550px]"
+            value={searchText}
+            onChange={handleSearchTextChange}
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                handleSearchTextChange();
+              }
+            }}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setTimeout(() => setShowSuggestions(false), 200)}
+          />
+          <button
+            className="px-4 py-2 bg-green-200 m-4 rounded-lg"
+            onClick={handleSearchTextChange}
+          >
+            Search
+          </button>
+
+          {showSuggestions && suggetions.length > 0 && (
+            <ul className="absolute bg-white top-[150px] left-[455px] border z-10 w-[550px] p-2 ">
               {suggetions.map((s) => (
-                <Link to={`/category/${s.info.id}`} key={s.info.id}>
-                  <li className="">
-                    <div className="flex space-x-4 my-2 border-b-2">
-                      <img
-                        className="w-16 h-16 mb-1 rounded-full"
-                        src={CDN_URL + s.info.cloudinaryImageId}
-                        alt={s.info.name}
-                      />
-                      <div>
-                        <p>{s.info.name}</p>
-                      </div>
+                <li key={s.text} className="cursor-pointer hover:bg-gray-100">
+                  <div className="flex space-x-4 my-2 border-b-2">
+                    <img
+                      className="w-16 h-16 mb-1 rounded-full"
+                      src={CDN_URL + s.suggestions.cloudinaryId}
+                      alt={s.suggestions.text}
+                    />
+                    <div>
+                      <p>{s.suggestions.text}</p>
                     </div>
-                  </li>
-                </Link>
+                  </div>
+                </li>
               ))}
             </ul>
           )}
         </div>
-        <div className="search m-4 p-4 flex items-center">
-          <button
-            className="px-4 py-2 bg-gray-100 rounded-lg"
-            onClick={() => {
-              const filteredList = listOfRestaurent.filter(
-                (res) => res.info.avgRating > 4.2
-              );
-              setFilteredRestaurants(filteredList);
-            }}
-          >
-            Top Rated Restaurant
-          </button>
-        </div>
+        {/* <div className="search m-4 p-4 flex items-center">
+      <button
+        className="px-4 py-2 bg-gray-100 rounded-lg"
+        onClick={() => {
+          const filteredList = listOfRestaurent.filter(
+            (res) => res.info.avgRating > 4.2
+          );
+          setFilteredRestaurants(filteredList);
+        }}
+      >
+        Top Rated Restaurant
+      </button>
+    </div> */}
       </div>
-
+      <div className="max-w-[1500px] mx-auto w-8/12">
+        <Category />
+      </div>
       <div className="flex flex-wrap">
-        {filteredRestaurants.map((restaurent, index) => (
-          <Link to={`/category/${restaurent.info.id}`} key={restaurent.info.id}>
+        {/* {filteredRestaurants.map((restaurent, index) => (
+      <Link to={`/category/${restaurent.info.id}`} key={restaurent.info.id}>
+        {" "}
+        <RestaurentCard key={index} resData={restaurent.info} />
+      </Link>
+    ))} */}
+
+        {/* {
+    filteredRestaurants.length > 0
+      ? filteredRestaurants.map((restaurent, index) => (
+          <Link
+            to={"/restaurants/" + restaurent.info.id}
+            key={restaurent.info.id}
+          >
             {" "}
             <RestaurentCard key={index} resData={restaurent.info} />
           </Link>
-        ))}
-
-        {/* {
-        filteredRestaurants.length > 0
-          ? filteredRestaurants.map((restaurent, index) => (
-              <Link
-                to={"/restaurants/" + restaurent.info.id}
-                key={restaurent.info.id}
-              >
-                {" "}
-                <RestaurentCard key={index} resData={restaurent.info} />
-              </Link>
-            ))
-          :
-           listOfRestaurent.map((restaurent) => (
-              <div className="duration-1000">
-                <Link
-                  to={"/restaurants/" + restaurent.info.id}
+        ))
+      :
+       listOfRestaurent.map((restaurent) => (
+          <div className="duration-1000">
+            <Link
+              to={"/restaurants/" + restaurent.info.id}
+              key={restaurent.info.id}
+            >
+              <div>
+                <RestaurentCard
                   key={restaurent.info.id}
-                >
-                  <div>
-                    <RestaurentCard
-                      key={restaurent.info.id}
-                      resData={restaurent.info}
-                    />
-                  </div>
-                </Link>
+                  resData={restaurent.info}
+                />
               </div>
-            )
-            )} */}
+            </Link>
+          </div>
+        )
+        )} */}
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDCxBM6Kcz0g23lJMp4TBdQvTVQD5x8urI",
+  authDomain: "swiggy-bae90.firebaseapp.com",
+  projectId: "swiggy-bae90",
+  storageBucket: "swiggy-bae90.appspot.com",
+  messagingSenderId: "259310876150",
+  appId: "1:259310876150:web:57e9b024ae0e4b1b13c3d7",
+  measurementId: "G-CRS1HBPT8B",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
